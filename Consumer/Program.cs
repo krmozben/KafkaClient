65c576133d8fa63c57a16ca1756261b842877074
kerim_ozben <kerim.ozben@consultant.morhipo.com>
2022-04-29 20:28:16 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Confluent.Kafka;
+
+
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "foo",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("ElasticsearchDataTopic");
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+    }
+
+    consumer.Close();
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+using System.Net;
+
+
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+
+        //  await producer.ProduceAsync("ElasticTopic", new Message<Null, string> { Value = message })
+        await producer.ProduceAsync(new TopicPartition("ElasticTopic",new Partition(1)), new Message<Null, string> { Value = message })
+        .ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Console.WriteLine(task.Exception.Message);
+            }
+            else
+            {
+                Console.WriteLine($"Wrote to offset: {task.Result.Offset}, partition: {task.Result.Partition}");
+            }
+        });
+
+        if (message.ToLower().Equals("exit"))
+            return;
+    }
+}
